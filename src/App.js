0460777e89732cab5d5498f85e4c01d99a0f7484
kzk-maeda <kzk.maeda0711@gmail.com>
2020-12-25 02:03:12 +0900

    basic project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+function add(a, b) {
+  return a + b
+}
 
 function App() {
+  const message = 'React';
+  const attrs = {
+    src: './logo.png',
+    alt: 'react'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {message}</p>
+      <p>1 + 2 = {add(1, 2)}</p>
+      <img {...attrs} />
     </div>
   );
 }
